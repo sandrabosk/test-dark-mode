6a0c60b30bce615f64680d010071da5a8f85e6eb
sandrabosk <sandraboskovic@hotmail.com>
2021-04-02 14:47:29 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Darkmode from "darkmode-js";
+
+const options = {
+  bottom: "64px", // default: '32px'
+  left: "unset", // default: '32px'
+  right: "32px", // default: 'unset'
+  time: "0.5s", // default: '0.3s'
+  mixColor: "#fff", // default: '#fff'
+  backgroundColor: "#fff", // default: '#fff'
+  buttonColorDark: "#100f2c", // default: '#100f2c'
+  buttonColorLight: "#fff", // default: '#fff'
+  saveInCookies: false, // default: true,
+  label: "🌓", // default: ''
+  autoMatchOsTheme: true // default: true
+};
+
+const darkmode = new Darkmode(options);
 
 function App() {
+  darkmode.showWidget();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header>
+        <img src={logo} className='App-logo' alt='logo' />
+        <h2>H2 something something</h2>
+
+        <br />
+
+        <p>This is test app.</p>
       </header>
     </div>
   );
